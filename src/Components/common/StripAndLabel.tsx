01b52ad2472386d52fd8eb8c911ca8f9cc439a9c
Urvesh109 <urveshi.t@gmail.com>
2024-06-20 21:31:48 +0530

    Add gradient support for stripLine

--- src/Components/common/StripAndLabel.tsx
+++ src/Components/common/StripAndLabel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import Svg, {Line} from 'react-native-svg';
+import {Svg, Rect, Defs, LinearGradient, Stop} from 'react-native-svg';
 import {getTopAndLeftForStripAndLabel} from 'gifted-charts-core';
 
 export const StripAndLabel = props => {
@@ -54,23 +54,28 @@ export const StripAndLabel = props => {
               ? 0
               : containerHeight - pointerStripHeight,
           }}>
-          <Svg>
-            <Line
-              stroke={pointerStripColor}
-              strokeWidth={pointerStripWidth}
-              strokeDasharray={
-                pointerConfig?.strokeDashArray
-                  ? pointerConfig?.strokeDashArray
-                  : ''
-              }
-              x1={0}
-              y1={0}
-              x2={0}
-              y2={
-                pointerStripUptoDataPoint
-                  ? containerHeight - pointerYLocal + 5 - xAxisThickness
-                  : pointerStripHeight
-              }
+          <Svg height="150" width="2">
+            <Defs>
+              <LinearGradient
+                id="defaultUnits"
+                x1="0%"
+                y1="0%"
+                x2="0%"
+                y2="100%">
+                <Stop offset="0%" stopColor="#000" stopOpacity="0.5" />
+                <Stop offset="50%" stopColor="#fff" stopOpacity="1" />
+                <Stop offset="80%" stopColor="#fff" stopOpacity="0.5" />
+                <Stop offset="100%" stopColor="#000" stopOpacity="0" />
+              </LinearGradient>
+            </Defs>
+            <Rect
+              fill="url(#defaultUnits)"
+              x="1"
+              y="1"
+              width="2"
+              height="140"
+              rx="1"
+              ry="1"
             />
           </Svg>
         </View>
